215494501860195b9ae0b425296684cc455c9ca1
valeriishevchuk <shevchuk.v.s@proton.me>
2024-11-27 17:27:31 +0200

    Add files via upload

---
+++ homework3.1.py
@@ -0,0 +1,11 @@
+user_number1 = int(input ("Введите 1-е число: "))
+user_number2 = int(input ("Введите 2-е число: "))
+user_action = input ("Введите действие с числами: (+, -, *, /): ")
+if user_action == "+":
+    print ("Ваш ответ: ", user_number1 + user_number2)
+if user_action == "-":
+    print ("Ваш ответ: ", user_number1 - user_number2)
+if user_action == "*":
+    print ("Ваш ответ: ", user_number1 * user_number2)
+if user_action == "/":
+    print("Ваш ответ: ", user_number1 / user_number2) if user_number2 != 0 else print("Деление на ноль невозможно!")
